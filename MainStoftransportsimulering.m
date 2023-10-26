5195067c2dbf3e1b96bd5419d2502f90df37e08d
Andemanden <82566176+Andemanden@users.noreply.github.com>
2023-10-26 10:41:15 +0200

    brughhhh

--- MainStoftransportsimulering.m
+++ MainStoftransportsimulering.m
@@ -5,25 +5,27 @@ clc %Clear command window
 tic %Clear elapsed time counter
 
 %% Decleration of startvariables
-N = 5; % Number of Control Volumes
-L = 0.02; %[m] % Domain length
+D = 0.1; % Diffusionskoefficient (tilpas efter behov)
+u = 1.0; % Hastighed (tilpas efter behov)
+rho = 1.0; % Densitet (tilpas efter behov)
+% Create domain
+L = 1.0; %[m] % Domain length
+N = 50; % Number of Control Volumes
+dx = L / N; % Cellestørrelse
+x = linspace(0, L, N + 1); % Cellegrænser
 h = L/(N); % Grid Spacing (Wall thickness)
-k = 10;  %[W/m-K] % Diffusivity (Thermal conductivity)
-Row_c = 10e6;  %[J/m^(3)-K] % Specific Heat Capacity
-q = 1000e3;     %  [W/m^(3)] % Uniform Heat Generation, A Source Term
+
+q = 1000e3;     % Viscosity
 % The center of the first cell is at dx/2 & the last cell is at L-dx/2.
 x = h/2 : h : L-(h/2); % Cross sectional area of the 1D domain
 A = 1;          %[m^2] 
 alpha = k/Row_c; % Diffusivity (Heat)
-t_Final = 8; % Simulation Time
-dt = 2; % Discrete Time Steps
-T_a = 0;    %[\circ c] % Left Surface Temperature
-T_b = 0;    %[\circ c] % Right Surface Temperature
-lambda = (alpha.*dt)/(h^2);  % Parameteric Setup
-% Initializing Variable
-T_Old = zeros(N,1); % Unknowns at time level n
-T_New = zeros(N,1); % Unknowns at time level n+1
-T_Old(:) = 200; % Initial Value (Initial Condition)
+tMax = 1.0; % Simuleringsvarighed
+dt = 2; % Discrete Time Steps (Descrete time increment)
+t = 0:dt:tMax;
+R=8.31415; % Gasconstant []
+C0 = zeros(1, N+1);
+C0(1) = 1.0; % Koncentrationen ved den ene ende
 
 %% Main
 
@@ -31,12 +33,12 @@ value=0;
 
 %MainStoftransportsimulering
 
-Arr=[]; %Main array
+Arr=[ 1 2 3]; %Main array
 
 %Main Forloop of array
-for i=1:2 %For looping through x-dimension
-    for j=1:2 %For looping through t-dimension
-    
+for i=1:length(Arr) %For looping through x-dimension
+    for j=1:length(Arr(j)) %For looping through t-dimension
+        
     end
 end
 
@@ -45,8 +47,13 @@ end
 Stoftransportsimuleringsapp %%The main script for GUI.mlapp
 
 %% Object decleration
-obj = Kontrolvol;
-obj = obj.MyClass(1);
+%obj1 = Kontrolvol;
+%obj1 = Kontrolvol.MyClass(1);
 
+%% Plotfunctions
+
+function data=loadData()
+    data=Arr;
+end
 
 
