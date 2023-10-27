beeb9cbce517ab5a585b6f47bf491569f594821c
Andemanden <82566176+Andemanden@users.noreply.github.com>
2023-10-27 08:53:06 +0200

    Bruhhh

--- numerical.m
+++ numerical.m
@@ -12,7 +12,7 @@ time_steps = 500;       % Number of time steps
 dt = time_length / time_steps;
 
 D = 0.01;               % Diffusion coefficient
-velocity = 0.1;         % Constant velocity (m/s)
+velocity = 0.6;         % Constant velocity (m/s)
 inflow_concentration = 1.0; % Constant solute concentration at the first cell
 
 % Create a grid for space and time
@@ -27,7 +27,7 @@ rejection_rate = 0; % No rejection
 
 % Initial condition (as a column vector)
 C(:, 1) = zeros(discretization, 1); % Initialize to 0 everywhere
-C(discretization/2, 1) = inflow_concentration; % Set the inflow concentration at the first position
+C(1, 1) = inflow_concentration; % Set the inflow concentration at the first position
 
 % Time-stepping loop
 for k = 2:time_steps
@@ -36,8 +36,15 @@ for k = 2:time_steps
         d2Cdx2 = D * (C(min(i + 1, discretization), k-1) - 2 * C(i, k-1) + C(max(i - 1, 1), k-1)) / dx^2;
         
         % Calculate convection term
-        dCdt_convection = -velocity * (C(i, k-1) - C(max(i - 1, 1), k-1)) / dx;
-        
+        if i == 1
+            % Apply advection and inflow at the first cell
+            dCdt_convection = -velocity * C(i, k-1) / dx;
+            C(1, k) = inflow_concentration; % Fill the first cell with inflow concentration
+        else
+            % Apply advection term
+            dCdt_convection = -velocity * (C(i, k-1) - C(i-1, k-1)) / dx;
+        end
+
         % Apply the diffusion-convection equation
         C(i, k) = C(i, k-1) + dt * (d2Cdx2 - rejection_rate * C(i, k-1) + dCdt_convection);
     end
