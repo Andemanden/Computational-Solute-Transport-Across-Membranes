61ff554d8f804c00c4cec23e4c58b0e06d6e4ecb
Andemanden <82566176+Andemanden@users.noreply.github.com>
2023-10-27 08:13:56 +0200

    step 1 and numerical

--- numerical.m
+++ numerical.m
@@ -12,6 +12,8 @@ time_steps = 500;       % Number of time steps
 dt = time_length / time_steps;
 
 D = 0.01;               % Diffusion coefficient
+velocity = 0.1;         % Constant velocity (m/s)
+inflow_concentration = 1.0; % Constant solute concentration at the first cell
 
 % Create a grid for space and time
 x = linspace(0, domain_length, discretization);
@@ -20,13 +22,12 @@ t = linspace(0, time_length, time_steps);
 % Initialize the concentration array (1D)
 C = zeros(discretization, time_steps);
 
-% Set up the diffusion ODE without rejection
-% There's no need for rejection_rate here
+% Set up the diffusion-convection ODE
 rejection_rate = 0; % No rejection
 
 % Initial condition (as a column vector)
-C(:, 1) = ones(discretization, 1); % Initialize to 1 everywhere
-C(1, 1) = 2; % Set the concentration at the first position to 2
+C(:, 1) = zeros(discretization, 1); % Initialize to 0 everywhere
+C(discretization/2, 1) = inflow_concentration; % Set the inflow concentration at the first position
 
 % Time-stepping loop
 for k = 2:time_steps
@@ -34,8 +35,11 @@ for k = 2:time_steps
         % Calculate the second derivative in x direction
         d2Cdx2 = D * (C(min(i + 1, discretization), k-1) - 2 * C(i, k-1) + C(max(i - 1, 1), k-1)) / dx^2;
         
-        % Apply the diffusion equation
-        C(i, k) = C(i, k-1) + dt * d2Cdx2;
+        % Calculate convection term
+        dCdt_convection = -velocity * (C(i, k-1) - C(max(i - 1, 1), k-1)) / dx;
+        
+        % Apply the diffusion-convection equation
+        C(i, k) = C(i, k-1) + dt * (d2Cdx2 - rejection_rate * C(i, k-1) + dCdt_convection);
     end
 end
 
@@ -46,6 +50,7 @@ plot(x, C(:, time_index));
 xlabel('Position (meters)');
 ylabel('Concentration');
 title(['Concentration Over 1D Domain at Time: ' num2str(t(time_index))]);
+
 
 % Create a 3D surface plot to visualize concentration over time and position
 [T, X] = meshgrid(t, x);
