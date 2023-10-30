6639848819b333a90eef5cf0ff4a186be95fa8dc
Andemanden <82566176+Andemanden@users.noreply.github.com>
2023-10-30 14:38:37 +0100

    with osmotic pressure

--- numerical.m
+++ numerical.m
@@ -15,16 +15,20 @@ dt = time_length / time_steps;
 D = 0.001;               % Diffusion coefficient
 %velocity = 0.2;         % Constant velocity (m/s)
 feed_conc = 1; % Constant solute concentration at the first cell
-
+TMP=7; %TMP: Transmembrane Pressure [bar]
+Lv=0.0002; %Water Permiability for all cells
+% Lv=k_w0*exp(-k_b*t)
 
 % Anonymous functions
-velocity_function = @(Conc) 0.2; % Velocity [m/s]
+%velocity_function = @(DeltaConc) Lv*(TMP-(1*R*T*DeltaConc)); % Velocity [m/s]
+%velocity_function2 = @(Conc) 0.2;
 fouling_rate = @(time) 0.0007*time; % σ_f (fouling over time) set to 0.0007*time or (2.2/(3+exp(-0.1*time)))^4
 rejection_rate = @(time) 0.1+fouling_rate(time); % σ_0+σ_f
 
 %Constants
 R=8.31415; % Gasconstant []
 T=273.15+20; %Temperature [K]
+F= 96.485;%Faraday[C/mol]
 
 % Create a grid for space and time
 x = linspace(0, domain_length, domain_steps);
@@ -34,12 +38,12 @@ t = linspace(0, time_length, time_steps);
 C = ones(domain_steps, time_steps);
 % Initial condition (as a column vector)
 C(domain_steps/2,1)=2;
-rho = velocity_function(T)*dx/dt;
+%rho = velocity_function(T)*dx/dt;
 r = D*dx/dt;
-fprintf(' r = %f \n rho = %f ', r, rho);
-if (1-2*r-rho)<0 
-   fprintf('ERROR: Stabilitetsfejl');
-end
+%fprintf(' r = %f \n rho = %f ', r, rho);
+%if (1-2*r-rho)<0 
+%   fprintf('ERROR: Stabilitetsfejl');
+%end
 
 %% Time-stepping loop
 for j = 2:time_steps
@@ -50,21 +54,21 @@ for j = 2:time_steps
             % No left neighbor at the first cell
             d2Cdx2 = D * (C(2, j-1) -  C(1, j-1)) / dx^2;
              % Apply advection and inflow at the first cell
-            dCdt_advection = -velocity_function(T) * C(i, j-1) / dx;
+            dCdt_advection = -(Lv*(TMP-(1*R*T*(C(domain_steps-1, j-1)-C(domain_steps, j-1))))) * C(i, j-1) / dx;
         elseif i == domain_steps %__Membrane Cell___
             % No right neighbor at the last cell and MEMBRANE
-            d2Cdx2 = (D * (C(domain_steps - 1, j-1) - C(domain_steps, j-1)) / dx^2) + C(i,j-1)*(rejection_rate(j))*velocity_function(T)/(dx);
+            d2Cdx2 = (D * (C(domain_steps - 1, j-1) - C(domain_steps, j-1)) / dx^2) + C(i,j-1)*(rejection_rate(j))*(Lv*(TMP-(1*R*T*(C(domain_steps-1, j-1)-C(domain_steps, j-1)))))/(dx);
         else    %__Normal Cells__
             % Calculate the second derivative normally
             d2Cdx2 = D * (C(i + 1, j-1) - 2 * C(i, j-1) + C(i - 1, j-1)) / dx^2;
             % Apply advection term
-            dCdt_advection = -velocity_function(T) * (C(i, j-1) - C(i-1, j-1)) / dx;
+            dCdt_advection = -(Lv*(TMP-(1*R*T*(C(domain_steps-1, j-1)-C(domain_steps, j-1))))) * (C(i, j-1) - C(i-1, j-1)) / dx;
         end
         % Apply the diffusion-advection-(electromigration) equation
         C(i, j) = C(i, j-1) + dt * (d2Cdx2 + dCdt_advection);
     end
 end
-
+%velocity_function((C(domain_steps-1, j-1)-C(domain_steps, j-1)))
 %% Plot 1 - 2D
 % Define fractions of time steps you want to visualize
 time_fraction = [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];  % For example, 0.1 corresponds to 10% of time steps
