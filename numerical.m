c62ac95c23370ca88376624daebeea50f186bc20
Analrexia <149059699+Analrexia@users.noreply.github.com>
2023-10-31 14:16:51 +0100

    Beautified and Stability adjusted

--- numerical.m
+++ numerical.m
@@ -3,7 +3,6 @@ clear
 close all
 clc
 %% Inital Parameters 
-% Define parameters
 domain_length = 0.5;   % Domain length (meters)
 domain_steps = 100;    % Number of spatial domain_steps points
 dx = domain_length / (domain_steps - 1);
@@ -12,42 +11,34 @@ time_length = 0.5;      % Time length (seconds)
 time_steps = 1000;       % Number of time steps
 dt = time_length / time_steps;
 
+% DEFINED VARIABLES
 D = 0.001;               % Diffusion coefficient
-%velocity = 0.2;         % Constant velocity (m/s)
 feed_conc = 1; % Constant solute concentration at the first cell
-TMP=15; %TMP: Transmembrane Pressure [bar]
+TMP=10; %TMP: Transmembrane Pressure [bar]
 kb=0.0002; % Fouling Constant
 kw=0.00021; % Initial water permiability
 
-% Anonymous functions
-fouling_rate = @(time) 0.0007*time; % σ_f (fouling over time) set to 0.0007*time or (2.2/(3+exp(-0.1*time)))^4
-rejection_rate = @(time) 0.1+fouling_rate(time); % σ_0+σ_f
-Lv= @(time) kw*exp(-kb*time); %Water Permiability for all cells
-
-%Constants
+% PHYSICAL CONSTANTS
 R=8.31415; % Gasconstant []
 T=273.15+20; %Temperature [K]
-F= 96.485;%Faraday[C/mol]
+F= 96.485;%Faraday[C/mol
+% Anonymous functions
+rejection_rate = @(time) 0.1; % σ_0+σ_f
+Lv= @(time) kw*exp(-kb*time); %Water Permiability for all cells
 
 % Create a grid for space and time
 x = linspace(0, domain_length, domain_steps);
 t = linspace(0, time_length, time_steps);
 
-% Initialize the concentration array (1D)
+% Initialize the concentration array AND Initial condition (1D)
 C = ones(domain_steps, time_steps);
-% Initial condition (as a column vector)
 C(domain_steps/2,1)=2;
-%rho = velocity_function(T)*dx/dt;
-r = D*dx/dt;
-%fprintf(' r = %f \n rho = %f ', r, rho);
-%if (1-2*r-rho)<0 
-%   fprintf('ERROR: Stabilitetsfejl');
-%end
+
 
 %% Time-stepping loop
 for j = 2:time_steps
     for i = 1:domain_steps
-        Jv = (Lv(j)*(TMP-(1*R*T*(C(domain_steps-1, j-1)-C(domain_steps, j-1)))));  % Velocity [m/s]
+        Jv = (Lv(j)*(TMP-(1*R*T*(C(domain_steps-1, j-1)-C(domain_steps, j-1)))));  % Volume flux = Velocity ,  in terms of osmotic pressure (TMP, R, T, delta_C) and Lv. [m/s]
         % Calculate the second derivative in x direction
         if i == 1 %__First Cell__
              C(1,:) = feed_conc; % Fill the first cell with inflow concentration
@@ -67,16 +58,29 @@ for j = 2:time_steps
         % Apply the diffusion-advection-(electromigration) equation
         C(i, j) = C(i, j-1) + dt * (d2Cdx2 + dCdt_advection);
         Jv_values(j) = Jv;
+        AS(j) = Jv * dt/dx;
     end
 end
-%velocity_function((C(domain_steps-1, j-1)-C(domain_steps, j-1)))
-%% Plot 1 - 2D
-% Define fractions of time steps you want to visualize
-time_fraction = [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];  % For example, 0.1 corresponds to 10% of time steps
 
-% Calculate the corresponding time indices
-time_instances = round(time_fraction * time_steps);
+%Diffusive Stability
+DS = D*dx/dt;
+fprintf(' Diffusivity Stability = %f', DS);
+if DS>0.5 
+   fprintf('ERROR: Stabilitetsfejl');
+else
+    fprintf('\n Stable Diffusion Model !!');
+end
 
+%% 2D Plots
+
+% Plot Advection Stability (DS) values over time
+
+figure;
+plot(t, AS);
+xlabel('Time (seconds)');
+ylabel('Advection Stability');
+title('Advection Stability Over Time');
+grid on;
 
 % Plot Jv values over time
 figure;
@@ -85,6 +89,12 @@ xlabel('Time (seconds)');
 ylabel('Jv (Velocity)');
 title('Jv (Velocity) Over Time');
 grid on;
+
+% Define fractions of time steps you want to visualize
+time_fraction = [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];  % For example, 0.1 corresponds to 10% of time steps
+
+% Calculate the corresponding time indices
+time_instances = round(time_fraction * time_steps);
 
 % Create a figure for the first plot
 figure;
@@ -107,7 +117,8 @@ grid on;
 hold off;
 
 
-%% Plot 2 - 3D
+%% 3D Plot
+
 % Create a 3D surface plot to visualize concentration over time and position
 [T, X] = meshgrid(t, x);
 figure;
