ef0657db47c7ab0c7485f4af971c469b37c40953
Analrexia <149059699+Analrexia@users.noreply.github.com>
2023-11-01 11:39:18 +0100

    STABLE RELEASE - 2 WARNINGS

--- numerical.m
+++ numerical.m
@@ -3,19 +3,19 @@ clear
 close all
 clc
 %% Inital Parameters 
-domain_length = 0.001;   % Domain length (meters)
+domain_length = 0.1;   % Domain length (meters)
 domain_steps = 100;    % Number of spatial domain_steps points
 dx = domain_length / (domain_steps - 1);
 
-time_length = 0.5;      % Time length (seconds)
+time_length = 500;      % Time length (seconds)
 time_steps = 5000;       % Number of time steps
 dt = time_length / time_steps;
 
 % DEFINED VARIABLES
 D = 0.846*10^-9; % Diffusivity coefficient H2PO4 in water
 feed_conc = 0.1; % Constant solute concentration at the first cell 0.1 molar [H2PO4]
-TMP= 12; %TMP: Transmembrane Pressure [bar]
-kb= 0; % Fouling Constant
+TMP= 5.5; %TMP: Transmembrane Pressure [bar]
+kb= 0.001; % Fouling Constant
 kw= 3.044001*10^-4; % Initial water permiability L m^-2 bar^-1 s^-1 
 rejection_rate = 0.1; % Rejection of the membrane (σ) [procentage]
 
@@ -38,7 +38,7 @@ t = linspace(0, time_length, time_steps);
 % Initialize the concentration array AND Initial condition (1D)
 C = zeros(domain_steps, time_steps)+0.1; % 0.1 molar [H2PO4]
 
-C(domain_steps/2,1)=0.3;
+C(domain_steps/2,1)=0.5;
 
 %Diffusive Stability
 DS = D*dx/dt;
@@ -54,7 +54,6 @@ end
 for j = 2:time_steps
     for i = 1:domain_steps
         Jv = (Lv(j)*(TMP-(1*R*T*(C(domain_steps, j-1)))));  % Volume flux = Jv ,  in terms of osmotic pressure (TMP, R, T, delta_C) and Lv. [m/s]
-                        %PROBLEM!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         C(1, :) = 0.1; % Set the leftmost boundary to 0.1
         % Calculate the second derivative in x direction
         if i == 1 %__First Cell__
