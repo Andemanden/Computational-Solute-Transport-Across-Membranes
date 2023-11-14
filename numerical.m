bbad2225b5b57cc1c59d62c3d7155572da64b6cc
Analrexia <149059699+Analrexia@users.noreply.github.com>
2023-11-14 10:26:52 +0100

    advection in percipitate

--- numerical.m
+++ numerical.m
@@ -65,7 +65,7 @@ for j = 2:time_steps
         if j == 2
             Ptot = InitP; % The rate of percipitation WI
         else
-            Ptot = Mp(LastC);
+            Ptot = Mp(LastC) + (Mp(LastC) - InitP)*Jv*(dt/dx);
         end
 
         Jv = (Lv(LastC)*(TMP-(1*R*T*(LastC))));  % Volume flux = Jv ,  in terms of osmotic pressure (TMP, R, T, delta_C) and Lv. [m/s]
