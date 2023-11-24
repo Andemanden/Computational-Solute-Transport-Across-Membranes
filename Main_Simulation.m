07772ebbd832880931809bcd95f755dcc6a5650f
Analrexia <149059699+Analrexia@users.noreply.github.com>
2023-11-24 15:10:54 +0100

    merged

--- Main_Simulation.m
+++ Main_Simulation.m
@@ -3,49 +3,49 @@ clear
 close all
 clc
 %% Inital Parameters
-Lx = 0.1;   % Domain length [m]
-domain_steps = 1000;    % Number of spatial domain steps
+Lx = 0.1;                     % Domain length [m]
+domain_steps = 1000;          % Number of spatial domain steps
 dx = Lx / (domain_steps - 1); % Position discretization [m]
  
-Lt = 500;      % Time length [s]
-time_steps = 500;       % Number of time steps
-dt = Lt / time_steps; % Temporal discretization [s]
+Lt = 500;              % Time length [s]
+time_steps = 500;      % Number of time steps
+dt = Lt / time_steps;  % Temporal discretization [s]
 
 % DEFINED VARIABLES
-D = 1.464*10^-9; % Diffusivity coefficient H2PO4- in water [m^2 s^-1]
-Cf = 0.1; % Ionconcentration of the feed [mol L^-1]
-DeltaP = 35; % Change in outer pressure [bar]
-
-Am = 0.0308; % Surfacearea of the membrane [m^2]
-k0 = 5.7311*10^(-7); % Initial water permeability [m^3 m^-2 bar^-1 s^-1] 
-mu = 0.8903*10^-9; % Viscosity of water [bar∙s]
-Rm = 1/(mu*k0); %  Membraneresitance of water [m^-1]
-alpha = 1*10^14; % Specific fouling resistance [m · kg-1] or [m L mol^-1]
-JuScalar = 0.5; %Percipitate Advection Coefficient
-
-sig_i = 0.1; % Rejection of ions 
+D = 1.464*10^-9;      % Diffusivity coefficient H2PO4- in water [m^2 s^-1]
+Cf = 0.1;             % Ionconcentration of the feed [mol L^-1]
+DeltaP = 35;          % Change in outer pressure [bar]
+Am = 0.0308;          % Surfacearea of the membrane [m^2]
+k0 = 5.7311*10^-7;    % Initial water permeability [m^3 m^-2 bar^-1 s^-1] 
+mu = 0.8903*10^-9;    % Viscosity of water [bar∙s]
+Rm = 1/(mu*k0);       % Membraneresitance of water [m^-1]
+alpha = 1*10^14;      % Specific fouling resistance [m · kg-1] or [m L mol^-1]
+JuScalar = 0.5;       % Percipitate Advection Coefficient
+sig_i = 0.1;          % Rejection of ions 
 
 % PHYSICAL CONSTANTS
-R = 8.31415*10^-2; % Gasconstant [L Bar mol^-1 K^-1]
-T = 273.15+25; %Temperature [K]
+
+R = 8.31415*10^-2;  % Gasconstant [L Bar mol^-1 K^-1]
+T = 273.15+25;      % Temperature [K]
  
 % Anonymous functions
 
-Udf = @(conc) 0.0011*exp(36.328*conc) + 0.263253; % Concentration of percipitation in respect to phosphate increase [mol L^-1]
-Rf = @(conc) alpha*(Udf(conc)/Am); % Foulingresistance [m^-1]
-k = @(conc) 1/(mu*(Rm+Rf(conc))); % Water permeability dependent on mu,Rm and Rf [m^3 m^-2 bar^-1 s^-1]
+Udf = @(conc) 0.0011*exp(36.328*conc) + 0.263253; % Concentration of percipitate in respect to Ciw. [mol L^-1]
+Rf = @(conc) alpha*(Udf(conc)/Am);                % Foulingresistance [m^-1]
+k = @(conc) 1/(mu*(Rm+Rf(conc)));                 % Water permeability dependent on mu,Rm and Rf [m^3 m^-2 bar^-1 s^-1]
 
 
-% Create a grid for space and time
+% Space and Time Grid
 x = linspace(0, Lx, domain_steps);
 t = linspace(0, Lt, time_steps);
 
-% Initialize the concentration array (1D)
-C = zeros(domain_steps, time_steps)+0.1; % 0.1 [mol L^-1] H2PO4- at pH 2.9
+% Initial concentration array AND Initial condition
+C = zeros(domain_steps, time_steps)+0.1; % 0.1 molar [H2PO4] at pH 2.9
 
 
 % Diffusive Stability
-DS = D*dt/dx^2; % Diffusive stability value
+
+DS = D*dt/dx^2;
 fprintf('\n Diffusivity Stability = %f', DS);
 if DS>0.5 
    fprintf(2,'\n ERROR: Stabilitetsfejl');
@@ -59,37 +59,36 @@ for j = 2:time_steps
     for i = 1:domain_steps
         Ciw = C(domain_steps, j-1); % Concentration of ions at the wall [mol L^-1]
 
-        C(1, :) = Cf; % Set the leftmost boundary to feedconstration [mol L^-1]
+        C(1, :) = Cf; % Set the leftmost boundary to Cf [mol L^-1]
 
-        if j == 2   
-            Cbw = Udf(Cf); % The rate of percipitation
+        if j == 2
+            Cbw = Udf(Cf); % Initial Percipitation
         else
-            Cbw = Udf(Ciw) + Jkonv*Udf(Cf)*(dt/dx)*JuScalar; % Concentration of percipitate from it's advection [mol L^-1]
+            Cbw = Udf(Ciw) + Jkonv*Udf(Cf)*(dt/dx)*JuScalar; % Total percipitate after advection [mol L^-1]
         end
 
         Jkonv = (k(Ciw)*(DeltaP-(1*R*T*(Ciw))));  % Volume flux = Jkonv , in terms of osmotic pressure (DeltaP, R, T, C_(i_w)) and k. [m/s]
 
         % Calculate the second derivative in x direction
-        if i == 1 %__First Cell__
-            Jdiff = 0; % Diffusive ionflux
-            Jadv = 0; % Advective flux
+        if i == 1 % First Cell (no left neighbor)
+            Jdiff = 0;       % Diffusive ionflux
+            Jadv = 0;        % Advective flux
 
-        elseif i == domain_steps %__Membrane Cell___
+        elseif i == domain_steps % Membrane wall cell (no right neighbor)
             % No right neighbor at the last cell and MEMBRANE
-            Jdiff = (D * (C(domain_steps - 1, j-1) - C(domain_steps, j-1))) / dx^2; % Diffusive ionflux [mol · m-2 · s-1]
+            Jdiff = (D * (C(domain_steps - 1, j-1) - C(domain_steps, j-1))) / dx^2;           % Diffusive ionflux [mol · m-2 · s-1]
             Jadv = -Jkonv * (C(domain_steps, j-1)*(1-sig_i) - C(domain_steps - 1, j-1)) / dx; % Advective flux [mol · m-2 · s-1]
 
-        else    %__Normal Cells__
+        else    % Bulk Cells
             % Calculate the second derivative normally
-            Jdiff = D * (C(i + 1, j-1) - 2 * C(i, j-1) + C(i - 1, j-1)) / dx^2; % Diffusive ionflux [mol · m-2 · s-1]
-            % Apply advection term
-            Jadv = -Jkonv * (C(i, j-1) - C(i-1, j-1)) / dx; % Advective flux [mol · m-2 · s-1]
+            Jdiff = D * (C(i + 1, j-1) - 2 * C(i, j-1) + C(i - 1, j-1)) / dx^2; % Diffusive ionflux at the membrane wall [mol · m-2 · s-1]
+            Jadv = -Jkonv * (C(i, j-1) - C(i-1, j-1)) / dx;                     % Advective flux at the membrane wall [mol · m-2 · s-1]
         end
-        % Apply the diffusion-advection-(electromigration) equation
-        C(i, j) = C(i, j-1) + dt * (Jdiff + Jadv); % Concentration of cell
-        Jkonv_values(j) = Jkonv; % Plot values
-        Cbw_values(j) = Cbw; 
-        AS(j) = Jkonv * dt/dx; % Stability plot values
+        % Apply the diffusion-advection equation
+        C(i, j) = C(i, j-1) + dt * (Jdiff + Jadv); % Apply the diffusion-advection equation [mol L^-1]
+        Jkonv_values(j) = Jkonv;                   % Storing convective flux values in respect to time
+        Cbw_values(j) = Cbw;                       % Storing precipitate values in respect to time
+        AS(j) = Jkonv * dt/dx;                     % Storing Stability plot values
     end
 end
 %% conservation and error
@@ -101,10 +100,10 @@ Ciw(end) = [];
 
 Systemdiff = [0, diff(sum(C*volprc,1))]; % Difference in system concentration per dt
 
-inflow = Cf*(Jkonv_values)*(dt/dx)*volprc; % In - mol
-outflow = Ciw.* Jkonv_values*(dt/dx)*volprc*(1-sig_i); % Out - mol
-infoutdiff = (inflow - outflow);
-ERROR = ((Systemdiff - infoutdiff).*Systemdiff.^(-1))*100; % The mass conservation error
+inflow = Cf*(Jkonv_values)*(dt/dx)*volprc;                 % Input [mol]
+outflow = Ciw.* Jkonv_values*(dt/dx)*volprc*(1-sig_i);     % Output [mol]
+infoutdiff = (inflow - outflow);                           % Difference in input and output
+ERROR = ((Systemdiff - infoutdiff).*Systemdiff.^(-1))*100; % Mass conservation error
 
 
 %% 2D Plots
@@ -155,12 +154,11 @@ time_fraction = [0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];  % F
 % Calculate the corresponding time indices
 time_instances = round(time_fraction * time_steps);
 
-% Create a figure for the first plot
+% Figure - Concentration over time sideplot
 figure;
 hold on;
 
-% Plot concentration at specific time instances
-for i = 1:length(time_instances)
+for i = 1:length(time_instances)   % Concentration at specific time instances
     time_index = time_instances(i);
     plot(x, C(:, time_index));
 end
@@ -170,8 +168,6 @@ ylabel('Koncentration [mol L^{-1}]');
 title('Koncentration over Position ved Forskellige Tidsfraktioner');
 xlim([0.099, Lx]);
 ylim([0.1, 0.12]);
-
-% Add a legend for clarity
 legend(arrayfun(@(f) ['t=', num2str(f)], time_fraction, 'UniformOutput', false));
 
 grid on;
@@ -180,25 +176,23 @@ hold off;
 
 %% 3D Plot
 
-% Create a 3D surface plot to visualize concentration over time and position
+% 3D surface plot - concentration over time and position
 [T, X] = meshgrid(t, x);
 figure;
-h = surf(X, T, C); % Transpose removed here
+h = surf(X, T, C);
 xlabel('Position [m]');
 ylabel('Tid [s]');
 zlabel('Koncentration [mol L^{-1}]');
 title('Koncentration Over Tid og Position');
-
-% Set axis limits to start at the origin
 xlim([0, Lx]);
 ylim([0, Lt]);
-zlim([0.1, 0.12]); % Assuming max(C(:)) is the maximum concentration in your data
-
+zlim([0.1, 0.12]);
 set(h,'LineStyle','none')
 colormap(jet)
 clim([0.1, 0.11])
 
-%% Organize
+
+%%
 load('step1jv.mat', 'Jv_values1');
 load('step2jv.mat', 'Jv_values2');
 load('step3jv.mat', 'Jv_values3');
@@ -222,9 +216,9 @@ plot(x(2:end), Jv_values2, 'LineWidth', 1.5);
 plot(x(2:end), Jv_values3, 'LineWidth', 1.5);
 plot(x(2:end), Jv_values4, 'LineWidth', 1.5);
 
-xlabel('Time (seconds)');
-ylabel('Jv (Velocity)');
-title('Jv (Velocity) Over Time');
+xlabel('Tid [s]');
+ylabel('J_{konv} [m s^{-1}]');
+title('J_{konv} [m s^{-1}] Over Tid');
 grid on;
 
 legend('Step 1', 'Step 2', 'Step 3', 'Step 4');
